d7ec351afc8491428a81231dc914325cf598002c
Kuran V Kumar <kurankumar@gmail.com>
2019-10-02 23:40:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,14 +20,13 @@ end
 
 long_planeteer_calls(planeteer_summon)
 
-potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
 def find_the_cheese(potential_cheese)
   cheeses = %w[gouda cheddar camembert]
 
-  potentially_cheesy_items.find do |maybe_cheese|
+  potential_cheese.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
 end
 
-find_the_cheese(potentially_cheesy_items)
+find_the_cheese(potential_cheese)
